38e23f977b406356df6a13982808b7335c36ba37
Michael Illert <michael@michaelillert.com>
2024-07-08 22:13:29 +0200

    1

--- linearmdel.py
+++ linearmdel.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
-
 #%%
 # get and prep data
 data_root = "https://github.com/ageron/data/raw/main/"
@@ -13,14 +12,12 @@ x = life_sat[["GDP per capita (USD)"]].values
 y = life_sat[["Life satisfaction"]].values
 print(x)
 print(y)
-
 #%%
 # visualize data
 life_sat.plot(kind='scatter', grid=True,
               x="GDP per capita (USD)", y="Life satisfaction")
 plt.axis([23500, 62500, 4, 9])
 plt.show()
-
 #%%
 # select a linear model
 model_LR = LinearRegression()
@@ -34,3 +31,4 @@ model_KNR.fit(x, y)
 X_new = [[37_655.2]]    # cyprus GDP per capita in 2020
 print("Linear Regression ", model_LR.predict(X_new))
 print("KNN Regression ", model_KNR.predict(X_new))
+#%%
